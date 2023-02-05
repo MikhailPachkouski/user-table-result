b90bbd6dc0a1b0e38881346a37fa27d81292c54a
Mikhail Pachkouski <mixxxa.mmf@gmail.com>
2023-02-05 22:25:13 +0300

    add TextField

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "../components/TextField";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+    const handleChange = ({ target }) => {
+        setData((prev) => ({ ...prev, [target.name]: target.value }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(data);
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Email"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+            />
+            <TextField
+                label="Password"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+            />
+            <button>Submit</button>
+        </form>
+    );
 };
 
 export default Login;
